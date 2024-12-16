fbf8f0fdf1d3e54f2966c05cc4552fd04258e23c
Sara.Barrows <sara.barrows@saint-gobain.com>
2024-12-16 15:15:05 -0500

    trying to get workflow to work

--- app.py
+++ app.py
@@ -24,35 +24,35 @@ client = gspread.authorize(creds)
 sheet_id = "1Xp3jzJsTYeyJ5dE-uqeB0soCpsEFic2FgQvSO4JJ3zk"
 
 
-app=dash.Dash(__name__)
-server=app.server
+# app=dash.Dash(__name__)
+# server=app.server
 
-app.layout = html.Div(
-    [
-        dcc.Input(placeholder='Write name here', id='input-val'),
-        html.Button('submit', id='button-id', n_clicks=0),
-        html.Div(id='output-val')
-    ]
-)
+# app.layout = html.Div(
+#     [
+#         dcc.Input(placeholder='Write name here', id='input-val'),
+#         html.Button('submit', id='button-id', n_clicks=0),
+#         html.Div(id='output-val')
+#     ]
+# )
 
 
-@app.callback(
-    Output('output-val', 'children'),
-    Input('button-id', 'n_clicks'),
-    State('input-val', 'value')
-)
-def callbk(n_clicks, input_val):
+# @app.callback(
+#     Output('output-val', 'children'),
+#     Input('button-id', 'n_clicks'),
+#     State('input-val', 'value')
+# )
+# def callbk(n_clicks, input_val):
     
-    if n_clicks > 0 and input_val:
+#     if n_clicks > 0 and input_val:
                 
-        sheet = client.open_by_key(sheet_id).sheet1
-        sheet.append_row([input_val,2])
-        #sheet.update_cell(1, 1, input_val)
+#         sheet = client.open_by_key(sheet_id).sheet1
+#         sheet.append_row([input_val,2])
+#         #sheet.update_cell(1, 1, input_val)
         
 
-        return f'{input_val} has been entered'
-    return ''
+#         return f'{input_val} has been entered'
+#     return ''
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
